17c11e29e71712afdeb2eae48d41169cc1b646df
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2018-09-06 11:42:08 +0200

    Preset sys_get_temp_dir() for temporary files of mPDF

--- src/AppBundle/Utils/PdfGenerator.php
+++ src/AppBundle/Utils/PdfGenerator.php
@@ -12,10 +12,21 @@ extends \Mpdf\Mpdf
         $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
         $fontDir = $defaultConfig['fontDir'];
 
+        /*
+         * mPDF is pre-configured to use <path to mpdf>/tmp as a directory
+         * to write temporary files (mainly for images).
+         * Write permissions must be set for read/write access for the tmp directory.
+         *
+         * As the default temp directory will be in vendor folder,
+         * is is advised to set custom temporary directory.
+         */
+        $options['tempDir'] = array_key_exists('tempDir', $options)
+            ? $options['tempDir']
+            : sys_get_temp_dir();
+
         $options['fontDir'] = array_key_exists('fontDir', $options)
             ? array_merge($options['fontDir'], $fontDir)
             : $fontDir;
-
 
         $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
         $fontdata = $defaultFontConfig['fontdata'];
